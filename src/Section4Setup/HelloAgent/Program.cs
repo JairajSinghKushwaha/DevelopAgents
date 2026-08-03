bc77d369db67cf9c7c5d486c5e6c84d9f7659a0e
JairajSinghKushwaha <kjairajsingh@gmail.com>
2026-08-03 22:13:11 +0530

    Implemented GPT 5 Mini a versatile AI assistant for work, learning, coding, and creative writing

--- src/Section4Setup/HelloAgent/Program.cs
+++ src/Section4Setup/HelloAgent/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+// Define the variables we extracted from Microsoft Foundry 
+var model = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-5-mini";
+
+// Admin > All Projects > Name (or Parent resource) > Endpoint (Base URL)
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") 
+                ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+
+//var apikey = Environment.GetEnvironmentVariable("AZURE_OPENAI_APIKEY")
+//    ?? throw new InvalidOperationException("AZURE_OPENAI_APIKEY is not set.");
+
+// Create the Agent using MFA
+AIAgent agent = new AzureOpenAIClient(
+   new Uri(endpoint), new AzureCliCredential())
+   .GetChatClient(model)
+   .AsAIAgent(instructions: "You are a friendly assistant. Keep your answers brief.");
+
+// Use for CI/CD scenarios, Get it from: Admin > All Projects > Parent resource > Api Key
+//AIAgent agent = new AzureOpenAIClient(
+//    new Uri(endpoint), new ApiKeyCredential(apikey))
+//    .GetChatClient(model)
+//    .AsAIAgent(instructions: "You are a friendly assistant. Keep your answers brief.");
+
+// Invoke the Agent 
+Console.WriteLine(await agent.RunAsync("What are the most famous cities in India?"));
